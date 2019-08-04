afdfe6108ad0df5b75c02ac420c617de0ca649e8
Adam Gajzlerowicz <nelf86@gmail.com>
2019-08-04 17:10:19 +0200

    woorking

--- src/app.js
+++ src/app.js
@@ -3,24 +3,31 @@
  */
 
 import React from 'react';
-import { SafeAreaView, StyleSheet, View } from 'react-native';
-import { TapGestureHandler } from 'react-native-gesture-handler';
+import { SafeAreaView, StyleSheet } from 'react-native';
+import { TapGestureHandler, State } from 'react-native-gesture-handler';
+import Animated from 'react-native-reanimated';
 import { colors } from './themes';
+
+const { View, event, Value, cond, eq } = Animated;
 
 class App extends React.Component {
   constructor(props) {
     super(props);
+    const state = new Value(-1);
+    this.onStateChange = event([
+      {
+        nativeEvent: { state },
+      },
+    ]);
+
+    this._opacity = cond(eq(state, State.BEGAN), 0.3, 1);
   }
 
   render() {
     return (
       <SafeAreaView style={styles.container}>
-        <TapGestureHandler
-          onHandlerStateChange={e => {
-            console.log(e);
-          }}
-        >
-          <View style={styles.box} />
+        <TapGestureHandler onHandlerStateChange={this.onStateChange}>
+          <View style={[styles.box, { opacity: this._opacity }]} />
         </TapGestureHandler>
       </SafeAreaView>
     );
