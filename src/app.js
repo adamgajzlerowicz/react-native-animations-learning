c42a7814206d31fa8746300b829dc1d932fc1289
Adam Gajzlerowicz <nelf86@gmail.com>
2019-08-11 21:12:36 +0200

    meh

--- src/app.js
+++ src/app.js
@@ -7,28 +7,18 @@ import { SafeAreaView, StyleSheet } from 'react-native'
 import { PanGestureHandler, State } from 'react-native-gesture-handler'
 import Animated from 'react-native-reanimated'
 import { colors } from './themes'
-import { runSpring } from './animations'
 
-const {
-  View,
-  event,
-  Value,
-  eq,
-  cond,
-  set,
-  defined,
-  Clock,
-  stopClock
-} = Animated
+const { View, event, Value, eq, cond, set, Clock, stopClock, call } = Animated
 
 class App extends React.Component {
   constructor(props) {
     super(props)
+    this.onDrop = this.onDrop.bind(this)
     this.translateX = new Value(0)
     this.translateY = new Value(0)
     const dragX = new Value(0)
     const dragY = new Value(0)
-    const state = new Value(-1)
+    const gestureState = new Value(-1)
     const dragVX = new Value(0)
 
     this.onGestureEvent = event([
@@ -37,7 +27,7 @@ class App extends React.Component {
           translationX: dragX,
           translationY: dragY,
           velocityX: dragVX,
-          state
+          state: gestureState
         }
       }
     ])
@@ -46,36 +36,42 @@ class App extends React.Component {
     const transX = new Value()
 
     this.translateX = cond(
-      eq(state, State.ACTIVE),
+      eq(gestureState, State.ACTIVE),
       [stopClock(clockX), set(transX, dragX), transX],
-      [
-        set(
-          transX,
-          cond(defined(transX), runSpring(clockX, transX, dragVX, 0), 0)
-        )
-      ]
+      [set(transX, new Value(0))]
     )
 
     const clockY = new Clock()
     const transY = new Value()
 
     this.translateY = cond(
-      eq(state, State.ACTIVE),
+      eq(gestureState, State.ACTIVE),
       [stopClock(clockY), set(transY, dragY), transY],
       [
-        set(
-          transY,
-          cond(defined(transY), runSpring(clockY, transY, dragVX, 0), 0)
-        )
+        cond(eq(gestureState, State.END), call([transX, transY], this.onDrop)),
+        set(transY, new Value(0))
       ]
     )
+  }
+
+  onDrop = ([x, y]) => {
+    console.log(x, y)
+  }
+
+  react = e => {
+    const { width, height, x, y } = e.nativeEvent.layout
+
+    this.top = y
+    this.bottom = y + height
+    this.left = x
+    this.right = x + width
   }
 
   render() {
     return (
       <SafeAreaView style={styles.container}>
         <View style={[styles.like, styles.dropArea]} />
-        <View style={[styles.dislike, styles.dropArea]} />
+        <View style={[styles.dislike, styles.dropArea]} onLayout={this.react} />
         <PanGestureHandler
           onGestureEvent={this.onGestureEvent}
           onHandlerStateChange={this.onGestureEvent}
