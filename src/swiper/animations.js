12e52c19cc3a8519d86a4c87c221de49b663719e
Adam Gajzlerowicz <nelf86@gmail.com>
2019-08-17 10:39:47 +0200

    meh

---
+++ src/swiper/animations.js
@@ -0,0 +1,137 @@
+import Animated, { Easing } from 'react-native-reanimated'
+import { State } from 'react-native-gesture-handler'
+import { Dimensions } from 'react-native'
+
+const {
+  Value,
+  eq,
+  cond,
+  set,
+  Clock,
+  clockRunning,
+  stopClock,
+  startClock,
+  log,
+  call,
+  abs,
+  and,
+  greaterThan,
+  lessThan,
+  block,
+  timing
+} = Animated
+
+const deviceWidth = Dimensions.get('window').width
+const distanceToVote = deviceWidth / 6
+const throwOutDistance = deviceWidth * 2
+const throwOutSpeed = 500
+
+const makeLikingValue = ({ gestureState, condition, hasVoted }) =>
+  cond(
+    eq(hasVoted, true),
+    new Value(0),
+    cond(
+      and(eq(gestureState, State.ACTIVE), condition),
+      [new Value(1)],
+      [new Value(0)]
+    )
+  )
+
+export const getIsLikingValue = ({ gestureState, dragValue, hasVoted }) =>
+  makeLikingValue({
+    gestureState,
+    condition: greaterThan(dragValue, distanceToVote),
+    hasVoted
+  })
+
+export const getIsDislikingValue = ({ gestureState, dragValue, hasVoted }) =>
+  makeLikingValue({
+    gestureState,
+    condition: lessThan(dragValue, -distanceToVote),
+    hasVoted
+  })
+
+const startCardClock = (clock, state, startValue) =>
+  block([
+    set(state.finished, 0),
+    set(state.time, 0),
+    set(state.position, startValue),
+    set(state.frameTime, 0),
+
+    startClock(clock)
+  ])
+
+export const dragInteraction = ({
+  gestureValue,
+  gestureState,
+  onLike = () => console.log('liked'),
+  onDislike = () => console.log('disliked'),
+  hasVoted
+}) => {
+  const returnValue = new Value(0)
+  const clock = new Clock()
+  const isDragging = new Value(false)
+
+  const state = {
+    finished: new Value(0),
+    position: new Value(0),
+    time: new Value(0),
+    frameTime: new Value(0)
+  }
+
+  const config = {
+    duration: new Value(100),
+    toValue: new Value(0),
+    easing: Easing.inOut(Easing.ease)
+  }
+
+  return block([
+    cond(
+      eq(hasVoted, false),
+      cond(
+        eq(gestureState, State.ACTIVE),
+        [set(isDragging, true), set(returnValue, gestureValue)],
+        [
+          cond(
+            eq(isDragging, true),
+            cond(
+              lessThan(abs(gestureValue), distanceToVote),
+              [
+                set(isDragging, false),
+                startCardClock(clock, state, gestureValue)
+              ],
+              [
+                cond(
+                  eq(hasVoted, false),
+                  cond(
+                    lessThan(0, gestureValue),
+                    [
+                      set(config.toValue, throwOutDistance),
+                      set(config.duration, throwOutSpeed),
+                      startCardClock(clock, state, gestureValue),
+                      call([], onLike),
+                      set(hasVoted, true)
+                    ],
+                    [
+                      set(config.toValue, -throwOutDistance),
+                      set(config.duration, throwOutSpeed),
+                      startCardClock(clock, state, gestureValue),
+                      call([], onDislike),
+                      set(hasVoted, true)
+                    ]
+                  )
+                )
+              ]
+            )
+          )
+        ]
+      )
+    ),
+    cond(clockRunning(clock), [
+      timing(clock, state, config),
+      set(returnValue, state.position),
+      cond(state.finished, stopClock(clock))
+    ]),
+    returnValue
+  ])
+}
---
+++ src/swiper/index.js
@@ -0,0 +1,224 @@
+/**
+ * @flow
+ */
+
+import React from 'react'
+import { StyleSheet, Image, Text } from 'react-native'
+import { PanGestureHandler } from 'react-native-gesture-handler'
+import Animated from 'react-native-reanimated'
+
+import { colors } from '../themes'
+import {
+  dragInteraction,
+  getIsLikingValue,
+  getIsDislikingValue
+} from './animations'
+
+const { View, event, Value, interpolate, multiply } = Animated
+
+class App extends React.Component {
+  gestureState = new Value(-1)
+
+  dragX = new Value(0)
+  dragY = new Value(0)
+  isLikingOpacity = new Value(0)
+  isDislikingOpacity = new Value(0)
+  hasVoted = new Value(false)
+
+  onGestureEvent = event([
+    {
+      nativeEvent: {
+        translationX: this.dragX,
+        translationY: this.dragY,
+        state: this.gestureState
+      }
+    }
+  ])
+  constructor(props) {
+    super(props)
+    const { dragY, gestureState, dragX, hasVoted } = this
+    const { onLike, onDislike } = this.props
+
+    this.translateX = dragInteraction({
+      gestureValue: dragX,
+      gestureState,
+      onLike,
+      onDislike,
+      hasVoted
+    })
+    this.translateY = dragInteraction({
+      gestureValue: dragY,
+      gestureState,
+      hasVoted
+    })
+    this.isLikingOpacity = getIsLikingValue({
+      gestureState,
+      dragValue: dragX,
+      hasVoted
+    })
+    this.isDislikingOpacity = getIsDislikingValue({
+      gestureState,
+      dragValue: dragX,
+      hasVoted
+    })
+  }
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <PanGestureHandler
+          onGestureEvent={this.onGestureEvent}
+          onHandlerStateChange={this.onGestureEvent}
+        >
+          <View style={styles.container}>
+            <View
+              style={[
+                styles.likeInfo,
+                styles.dislikingInfo,
+                {
+                  opacity: this.isDislikingOpacity
+                }
+              ]}
+            />
+            <View
+              style={[
+                styles.likeInfo,
+                styles.likingInfo,
+                {
+                  opacity: this.isLikingOpacity
+                }
+              ]}
+            />
+
+            <View
+              style={[
+                styles.box,
+                {
+                  transform: [
+                    {
+                      translateX: multiply(this.translateX, 1.5),
+                      translateY: this.translateY,
+                      rotate: multiply(this.translateX, 0.0009)
+                    }
+                  ]
+                }
+              ]}
+            >
+              <Image
+                source={require('./image.png')}
+                style={styles.background}
+              />
+            </View>
+
+            <View
+              style={[
+                styles.overlayContainer,
+                {
+                  opacity: this.isLikingOpacity
+                }
+              ]}
+            >
+              <View
+                style={[
+                  styles.opaqueBackground,
+                  styles.likingOverlay,
+                  {
+                    opacity: interpolate(this.isLikingOpacity, {
+                      inputRange: [0, 1],
+                      outputRange: [0, 0.7]
+                    })
+                  }
+                ]}
+              />
+              <Text style={styles.textStyle}>Keep it</Text>
+            </View>
+
+            <View
+              style={[
+                styles.overlayContainer,
+                {
+                  opacity: this.isDislikingOpacity
+                }
+              ]}
+            >
+              <View
+                style={[
+                  styles.opaqueBackground,
+                  styles.dislikingOverlay,
+                  {
+                    opacity: interpolate(this.isDislikingOpacity, {
+                      inputRange: [0, 1],
+                      outputRange: [0, 0.7]
+                    })
+                  }
+                ]}
+              />
+              <Text style={styles.textStyle}>Return it</Text>
+            </View>
+          </View>
+        </PanGestureHandler>
+      </View>
+    )
+  }
+}
+
+const styles = StyleSheet.create({
+  textStyle: {
+    fontFamily: 'Avenir',
+    fontSize: 40,
+    color: colors.white,
+    fontWeight: '500'
+  },
+  likeInfo: {
+    top: 0,
+    left: 0,
+    width: '100%',
+    height: '100%',
+    position: 'absolute'
+  },
+  likingInfo: {
+    backgroundColor: colors.green
+  },
+  dislikingInfo: {
+    backgroundColor: colors.red
+  },
+  overlayContainer: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    top: 0,
+    left: 0,
+    width: '100%',
+    height: '100%',
+    position: 'absolute'
+  },
+  opaqueBackground: {
+    top: 0,
+    left: 0,
+    width: '100%',
+    height: '100%',
+    position: 'absolute'
+  },
+  likingOverlay: {
+    backgroundColor: colors.green
+  },
+  dislikingOverlay: {
+    backgroundColor: colors.red
+  },
+  background: {
+    width: '100%',
+    height: '100%',
+    resizeMode: 'cover'
+  },
+  container: {
+    position: 'relative',
+    alignItems: 'stretch',
+    justifyContent: 'center',
+    flex: 1
+  },
+  box: {
+    backgroundColor: colors.light,
+    flex: 1
+  }
+})
+
+export default App
